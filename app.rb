eddc2c7f4553e69f7c0eba5f1a40297d83c62e41
Anthony Graniero <ananfro@gmail.com>
2019-02-01 17:51:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "applesauce" 
+  end
+  
+  get '/' do
+    erb :index
+  end
+  
+  
 end
